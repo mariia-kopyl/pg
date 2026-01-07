ddad53c9b1708e496fd7443b4652f4fe241c6f66
mariia-kopyl <kopylm00@jcu.cz>
2026-01-07 11:58:03 +0000

    ex1.py

---
+++ ex1.py
@@ -0,0 +1,39 @@
+# napište funkci filtruj_cisla(typ, cisla), která přijme dva parametry:
+# typ – řetězec, který může nabývat hodnot "kladna", "zaporna", "suda", "licha"
+# cisla – seznam čísel
+# Funkce vrátí nový seznam obsahující pouze ta čísla z parametru cisla, která odpovídají zadanému typu.
+
+def filtruj_cisla(typ, cisla):
+    vysledek = []
+    if typ == "kladna":
+        for cislo in cisla:
+            if cislo>0:
+                vysledek.append(cislo)
+            else: continue
+    elif typ == "zaporna":
+        for cislo in cisla:
+            if cislo<0:
+                vysledek.append(cislo)
+            else: continue
+    elif typ == "suda":
+        for cislo in cisla:
+            if cislo % 2 == 0:
+                vysledek.append(cislo)
+            else: continue
+    elif typ == "licha": 
+        for cislo in cisla:
+            if cislo%2 != 0:
+                vysledek.append(cislo)
+            else: continue
+    else: return vysledek
+ 
+    return vysledek
+
+
+if __name__ == "__main__":
+    print(filtruj_cisla("kladna", [1, -2, 0, 5, -3]))   # [1, 5]
+    print(filtruj_cisla("suda", [1, 2, 3, 4, 5]))       # [2, 4]
+    print(filtruj_cisla("zaporna", [1, -2, 0, 5, -3]))   # [-2, -3]
+    print(filtruj_cisla("licha", [1, 2, 3, 4, 5]))       # [1, 3, 5]
+    # neexistující typ
+    print(filtruj_cisla("xxx", [1, 2, 3]))              # []
