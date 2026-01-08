e1892f3456f1947d5fe60bfc2b349dcee8010fb9
mariia-kopyl <kopylm00@jcu.cz>
2026-01-08 13:20:45 +0000

    zkouska1.py

---
+++ zkouska_2026_01_08/zkouska1.py
@@ -0,0 +1,36 @@
+# Příklad 1: Práce s podmínkami a cykly
+# Zadání:
+# Napište funkci `process_numbers`, která přijme seznam celých čísel. 
+# Funkce vrátí nový seznam, který obsahuje pouze čísla větší než 5, vynásobená 2.
+# Pokud při procházení seznamu narazíte na cokoli jiného než je číslo (tzn. int nebo float), ukončete zpracování seznamu a vraťte dosud vytvořený seznam.
+#
+# Vaše řešení můžete otestovat pomocí pytest takto:
+# pytest zkouska1.py
+# pokud Vám pytest nazahlásí žádné chyby, máte hotovo!
+#
+# instalace pytest:
+# pip install pytest
+
+
+def process_numbers(numbers):
+    # ZDE NAPIŠTE VÁŠ KÓD
+    vysledek = []
+    for cislo in numbers:
+        if type(cislo) not in (int, float):
+            break
+        if cislo > 5:
+            vysledek.append(cislo*2)
+    return vysledek
+
+
+# Unit testy
+def test_process_numbers():
+    assert process_numbers([1, 6, 3, '5', 8]) == [12]
+    assert process_numbers([7, 8, None, 12]) == [14, 16]
+    assert process_numbers([1, 2, 3, 4]) == []
+    assert process_numbers([5, 6, 7, 15]) == [12, 14, 30]
+    assert process_numbers([True, 4, 8, 10, 15]) == []
+
+
+if __name__ == "__main__":
+    test_process_numbers()
